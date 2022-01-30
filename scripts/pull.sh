fb10d3df862f423f30bd3e7573c08fcbc9d68060
47717_harsh <harshkoria@gmail.com>
2022-01-30 16:11:01 +0530

    aaaaaa

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+rm -rf /home/hkoria/Documents/exam_jenkins
 mkdir /home/hkoria/Documents/exam_jenkins
 cd /home/hkoria/Documents/exam_jenkins
 git clone https://github.com/koriaharsh/devops-210940120074.git
-cd devops-210940120074
+cd ./devops-210940120074
 javac myFile.java
 java myFile
